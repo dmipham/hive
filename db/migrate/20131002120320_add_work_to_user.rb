65e7402202ecf89d0ee0d917b01aef8ea286126b
Diemmi <diemmipham@gmail.com>
2013-10-08 17:06:37 -0400

    datatype change drop

--- db/migrate/20131002120320_add_work_to_user.rb
+++ db/migrate/20131002120320_add_work_to_user.rb
@@ -1,14 +1,14 @@
 class AddWorkToUser < ActiveRecord::Migration
   def change
-    add_column :users, :work_type_moving, :string
-    add_column :users, :work_type_cleaning, :string
-    add_column :users, :work_type_painting, :string
-    add_column :users, :work_type_yard, :string
-    add_column :users, :work_type_it, :string
-    add_column :users, :work_type_tutor, :string
-    add_column :users, :work_type_babysit, :string
-    add_column :users, :work_type_event, :string
-    add_column :users, :work_type_errands, :string
-    add_column :users, :work_type_maintenance, :string
+    add_column :users, :work_type_moving, :boolean
+    add_column :users, :work_type_cleaning, :boolean
+    add_column :users, :work_type_painting, :boolean
+    add_column :users, :work_type_yard, :boolean
+    add_column :users, :work_type_it, :boolean
+    add_column :users, :work_type_tutor, :boolean
+    add_column :users, :work_type_babysit, :boolean
+    add_column :users, :work_type_event, :boolean
+    add_column :users, :work_type_errands, :boolean
+    add_column :users, :work_type_maintenance, :boolean
   end
 end
